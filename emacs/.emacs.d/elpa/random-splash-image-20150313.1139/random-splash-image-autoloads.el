e22e8eaacb521108b87f09b461c28c0d554a849c
Jaroslav Pelikan <jdf@linuxmail.org>
2015-05-15 08:52:50 -0600

    removed .emacs.d

--- emacs/.emacs.d/elpa/random-splash-image-20150313.1139/random-splash-image-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; random-splash-image-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("random-splash-image.el") (21768 21772
-;;;;;;  64874 509000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; random-splash-image-autoloads.el ends here
--- emacs/.emacs.d/elpa/xkcd-20140505.511/xkcd-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "xkcd" "20140505.511" "View xkcd from Emacs" '((json "1.3")) :url "https://github.com/vibhavp/emacs-xkcd" :keywords '("xkcd" "webcomic"))
